f2129ec35a2e64ffc0bd34ed0e1c8715bfe05d05
Vinícius Santos <viniciusssantos.pa@gmail.com>
2024-12-28 16:58:30 -0300

    feat: bag items fix

--- client/src/components/geral/header/bag-component/BagComponent.tsx
+++ client/src/components/geral/header/bag-component/BagComponent.tsx
@@ -2,8 +2,10 @@ import { cartService } from "../../../../services/cartService";
 import { AccountIcon, SacolaIcon } from "../../../../components/icons/icons"
 import { Button } from "../../../../components/ui/button"
 import { DrawerBackdrop, DrawerBody, DrawerCloseTrigger, DrawerContent, DrawerFooter, DrawerHeader, DrawerRoot, DrawerTitle, DrawerTrigger } from "../../../../components/ui/drawer"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Product } from "../../../../models/product";
+import { TrashIcon } from "lucide-react";
+import { DataListItem, DataListRoot } from "../../../../components/ui/data-list";
 
 interface BagComponentProps {
     setBagOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,7 +13,34 @@ interface BagComponentProps {
 }
 
 const BagComponent: React.FC<BagComponentProps> = ({ setBagOpen, isBagOpen }) => {
-    const bagItems = cartService.get();
+    const [bagItems, setBagItems] = useState<Product[]>(cartService.get());
+
+    useEffect(() => {
+        setBagItems(cartService.get());
+    }, [isBagOpen]);
+
+    const handleRemove = (productId: any) => {
+        cartService.remove(productId);
+        setBagItems(cartService.get());
+    };
+
+    // Cálculo do subtotal, desconto e total
+    const subtotal = bagItems.reduce((acc, item) => {
+        const priceWithDiscount = item.price - item.desconto;
+        return acc + priceWithDiscount;
+    }, 0);
+
+    const totalDiscount = bagItems.reduce((acc, item) => {
+        return acc + item.desconto;
+    }, 0);
+
+    const total = subtotal - totalDiscount;
+
+    const stats = [
+        { label: "Subtotal", value: `R$ ${subtotal.toFixed(2)}`, helpText: "Valor dos pedidos" },
+        { label: "Desconto", value: `R$ ${totalDiscount.toFixed(2)}`, helpText: "Valor dos descontos" },
+        { label: "Total", value: `R$ ${total.toFixed(2)}`, helpText: "Valor total da compra" },
+    ];
 
     return (
         <DrawerRoot>
@@ -22,9 +51,8 @@ const BagComponent: React.FC<BagComponentProps> = ({ setBagOpen, isBagOpen }) =>
                 </Button>
             </DrawerTrigger>
             <DrawerContent className="drawer-header-component">
-                <DrawerCloseTrigger />
                 <DrawerHeader className="drawer-header-component__header">
-                    <DrawerTitle>Meus acessórios</DrawerTitle>
+                    <DrawerTitle>{bagItems.length === 0 ? "Meus acessórios" : `MINHA SACOLA (${bagItems.length})`}</DrawerTitle>
                 </DrawerHeader>
                 <DrawerBody className="drawer-header-component__body">
                     {bagItems.length === 0 ? (
@@ -35,29 +63,49 @@ const BagComponent: React.FC<BagComponentProps> = ({ setBagOpen, isBagOpen }) =>
                         </section>
                     ) : (
                         <section className="bag-items">
-                            {bagItems.map((item: Product) => (
-                                <div key={item.id + item.size} className="bag-item">
-                                    <div className="bag-item__details">
-                                        <p className="bag-item__name">{item.name_product}</p>
-                                        <p className="bag-item__size">Tamanho: {item.size}</p>
-                                        <p className="bag-item__price">{`R$ ${item.price.toFixed(2)}`}</p>
+                            {bagItems.map((item: Product) => {
+                                const priceWithDiscount = item.price - item.desconto;
+                                return (
+                                    <div key={item.id + item.size} className="bag-item">
+                                        <div className="image-bag-item">
+                                            <img src={JSON.parse(item.photoURL)[0]} alt={item.name_product} />
+                                        </div>
+                                        <div className="bag-item__details">
+                                            <p className="bag-item__name">{item.name_product}</p>
+                                            <p className="bag-item__size">Tamanho: {item.size}</p>
+                                            <p className="bag-item__price">
+                                                {`R$ ${priceWithDiscount.toFixed(2)}`}
+                                            </p>
+                                        </div>
+                                        <div>
+                                            <Button
+                                                onClick={() => handleRemove(item.id)}
+                                                variant="ghost"
+                                                aria-label="Remover item da sacola"
+                                            >
+                                                <TrashIcon size={16} />
+                                            </Button>
+                                        </div>
                                     </div>
-                                    <Button
-                                        onClick={() => cartService.remove(item.id)}
-                                        variant="ghost"
-                                        aria-label="Remover item da sacola"
-                                    >
-                                        Remover
-                                    </Button>
-                                </div>
-                            ))}
+                                );
+                            })}
                         </section>
                     )}
                 </DrawerBody>
-                <DrawerFooter />
+                {bagItems.length === 0 ? null :
+                    <DrawerFooter className="footerBag">
+                        <DataListRoot unstyled size={"md"} width={"full"} className="footerItems" orientation="horizontal">
+                            {stats.map((item) => (
+                                <DataListItem className="itemFooter" key={item.label} label={item.label} value={item.value} />
+                            ))}
+                        </DataListRoot>
+                        <Button className="finalizeBtn">FINALIZAR COMPRA</Button>
+                    </DrawerFooter>
+                }
+                <DrawerCloseTrigger />
             </DrawerContent>
         </DrawerRoot>
-    )
+    );
 }
 
 export default BagComponent;
