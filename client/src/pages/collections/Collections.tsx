4725cf0e4ecd9fcbbba8eb058aab581d8b9126c4
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-03-02 22:09:46 -0300

    feat: added loading page for collections and error tratment

--- client/src/pages/collections/Collections.tsx
+++ client/src/pages/collections/Collections.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Categories from "../categories/Categories";
-import { CategoriesProps, defaultsCategories } from "../../lib/utils";
+import { CategoriesProps } from "../../lib/utils";
 import { useParams } from "react-router-dom";
 import Footer from "../../components/geral/footer/Footer";
 import { adminService } from "../../services/adminService";
+import { Loader } from "../../components/ui/loader";
 
 const Collections = () => {
     const { collection_name } = useParams();
     const [categorie, setCategorie] = useState<CategoriesProps>()
+    const [categoryNull, setCategoryNull] = useState<boolean>(false);
 
     useEffect(() => {
         getCollections()
@@ -15,20 +17,30 @@ const Collections = () => {
 
     async function getCollections() {
         const categorys = await adminService.getAllCategoriesData();
-        setCategorie(categorys?.find((category: CategoriesProps) => category.urlLink === collection_name))
+        const foundCategory = categorys?.find((category: CategoriesProps) => category.urlLink === collection_name);
+        setCategorie(foundCategory);
+
+        if (!foundCategory) {
+            setCategoryNull(true);
+        }
+    }
+
+    let content;
+
+    if (categorie) {
+        content = <Categories CategoryHeaderContent={categorie} />;
+    } else if (categoryNull) {
+        window.location.href = window.location.origin + "/404";
+        content = null;
+    } else {
+        content = <Loader />;
     }
 
     return (
         <React.Fragment>
             <section className="collections-page-content">
                 <div className="collections-page-wrapper">
-                    {categorie ?
-                        <Categories
-                            CategoryHeaderContent={categorie}
-                        />
-                        :
-                        <h1>Categoria não encontrada.</h1>
-                    }
+                    {content}
                 </div>
             </section>
             <Footer />
