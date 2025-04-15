026c0d19e0f674d2fbc16b323802f11ccf91d0cd
Vinícius Santos <viniciusssantos.pa@gmail.com>
2025-04-15 14:47:58 -0300

    fix ambient config

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -7,7 +7,18 @@ const api = axios.create();
 const whiteListUrls = [
     process.env.REACT_APP_API_UPLOAD_URL,
     process.env.REACT_APP_API_ENDPOINT_APPWRITE
-]
+];
+
+const getUrlByAmbient = (): any => {
+    switch (process.env.REACT_APP_DEFAULTCONFIGURATION) {
+        case "production":
+            return process.env.REACT_APP_API_ENDPOINT_PRODUCTION;
+        case "local":
+            return process.env.REACT_APP_API_ENDPOINT;
+        default:
+            return "";
+    }
+}
 
 api.interceptors.request.use(config => {
     if (whiteListUrls.includes(config.url)) return config;
@@ -15,5 +26,6 @@ api.interceptors.request.use(config => {
     
     return config;
 });
-
+export { getUrlByAmbient };
+    
 export default api;
